d641ff8bb419522140c8db99900487fe80d2846c
samialab <6316945+samialab@users.noreply.github.com>
2019-04-03 15:46:05 +0100

    Add DQN state-action space

--- rlcache/strategies/caching_strategies/rl_caching_strategy.py
+++ rlcache/strategies/caching_strategies/rl_caching_strategy.py
@@ -1,14 +1,14 @@
-from typing import Dict
+from typing import Dict, List
 
 import numpy as np
 from rlgraph.agents import Agent
-from rlgraph.spaces import BoolBox, IntBox, TextBox
-from rlgraph.spaces import Dict as RLGraphDict
+from rlgraph.spaces import FloatBox, IntBox
 
 from rlcache.cache_constants import CacheStatus
 from rlcache.observers.observer import Observer
 from rlcache.rl_model.converter import RLConverter
 from rlcache.strategies.caching_strategies.caching_strategy_base import CachingStrategy
+from rlcache.utils.vocabulary import Vocabulary
 
 
 class RLCachingStrategy(CachingStrategy):
@@ -18,17 +18,17 @@ class RLCachingStrategy(CachingStrategy):
 
         num_indexes = config['num_fields']  # TODO for now only 1 state, the key, only
         agent_config = config['agent_config']  # DQN agent config
-        max_capacity = config['max_capacity']
+
+        flattened_num_cols = 1 + num_indexes + 1 + 1  # num_indexes + key + capacity + cache_status
 
         # TODO encode field = 0 capacity, field 1 = key, field 2 until num_indexes = values.
         # action space: should cache: true or false
         # state space:
+
+        # TODO state space and action_space are floatbox and intbox because bug in rlgraph.
         self.agent = Agent.from_spec(agent_config,
-                                     state_space=RLGraphDict({'key': TextBox(shape=(1, 1)),
-                                                              'values': TextBox(shape=(num_indexes, 1)),
-                                                              'capacity': IntBox(max_capacity)
-                                                              }),
-                                     action_space=BoolBox())
+                                     state_space=FloatBox(shape=(flattened_num_cols,)),
+                                     action_space=IntBox(2, shape=(1,)))
 
     def observer(self) -> Observer:
         # should maintain a dict of incomplete experiences, once it is complete, pop and put it in the agent memory.
@@ -38,29 +38,45 @@ class RLCachingStrategy(CachingStrategy):
         pass
 
     def should_cache(self, key: str, values: Dict[str, str], cache_status: CacheStatus) -> bool:
-        # call converter to transform the args to states.
         # query the agent.
         # transform agent action to system results (bool).
         # return that.
         # save the action taken, and the experience
+
+        # call converter to transform the args to states.
         state = self.converter.system_to_agent_state(key=key, values=values, cache_status=cache_status)
-        return self.agent.get_action(state)
+        agent_prediction = self.agent.get_action(state)
+
+        return self.converter.agent_to_system_action(agent_prediction)
 
 
 class CachingStrategyRLConverter(RLConverter):
-    def system_to_agent_state(self, *args, **kwargs) -> np.ndarray:
-        # TODO replace with meaningful state representation
-        capacity = 50  # TODO get this from observer
-        return np.array({'key': kwargs.get('key'),
-                         'values': kwargs.get('values'),
-                         'capacity': capacity
-                         })
+    def __init__(self, padding_size: int = 10):
+        self.padding_size = padding_size
+        self.vocabulary = Vocabulary()
+        # values_key_vocab: TODO this is to maintain to and from dict that translate result sets' keys
+        # self.values_key_vocabulary = Vocabulary(add_pad=False, add_unk=False)
 
-    def system_to_agent_action(self, *args, **kwargs):
+    def system_to_agent_state(self, key, values, cache_status) -> np.ndarray:
+        capacity = 50  # TODO get this from observer
+        key_encoded = self.vocabulary.add_or_get_id(key)
+        values_encoded = self.vocabulary.get_id_or_add_multiple(self._extracted_values_ordered(values),
+                                                                self.padding_size)
+        cache_status_encoded = self.vocabulary.add_or_get_id(cache_status)
+        # return np.random.randint(13, size=13)
+        return np.concatenate([np.array([key_encoded, capacity, cache_status_encoded]), values_encoded])
+
+    def system_to_agent_action(self, *args, **kwargs) -> np.ndarray:
         return np.ones(1)
 
-    def agent_to_system_action(self, actions, **kwargs):
-        pass
+    def agent_to_system_action(self, actions: np.ndarray, **kwargs) -> bool:
+        return (actions.flatten() == 1).item()
 
     def system_to_agent_reward(self, *args, **kwargs):
         pass
+
+    def _extracted_values_ordered(self, values: Dict[str, any]) -> List[any]:
+        sorted_values_by_key = []
+        for k in sorted(values.keys()):
+            sorted_values_by_key.append(values[k])
+        return sorted_values_by_key
---
+++ rlcache/utils/vocabulary.py
@@ -0,0 +1,103 @@
+from typing import Dict, Sized, List, FrozenSet, Optional
+
+import numpy as np
+
+
+# Taken and edited from https://github.com/Microsoft/dpu-utils/blob/master/dpu_utils/mlutils/vocabulary.py
+class Vocabulary(Sized):
+    """
+    A simple vocabulary that maps strings to unique ids (and back).
+    """
+
+    def __init__(self, add_unk: bool = True, add_pad: bool = True) -> None:
+        self.token_to_id = {}  # type: Dict[str, int]
+        self.id_to_token = []  # type: List[str]
+        if add_pad:
+            self.add_or_get_id(self.get_pad())
+        if add_unk:
+            self.add_or_get_id(self.get_unk())
+
+    def is_equal_to(self, other) -> bool:
+        """
+        This would be __eq__, except that Python 3 insists that __hash__ is defined if __eq__ is
+        defined, and we can't define __hash__ because the object is mutable.
+        """
+        if not isinstance(other, Vocabulary):
+            return False
+        return self.id_to_token == other.id_to_token
+
+    def add_or_get_id(self, token: str) -> int:
+        """
+        Get the token's id. If the token does not exist in the
+        dictionary, add it.
+        """
+        this_id = self.token_to_id.get(token)
+        if this_id is not None:
+            return this_id
+
+        this_id = len(self.id_to_token)
+        self.token_to_id[token] = this_id
+        self.id_to_token.append(token)
+        return this_id
+
+    def is_unk(self, token: str) -> bool:
+        return token not in self.token_to_id
+
+    def get_id_or_unk(self, token: str) -> int:
+        this_id = self.token_to_id.get(token)
+        if this_id is not None:
+            return this_id
+        else:
+            return self.token_to_id[self.get_unk()]
+
+    def get_id_or_add_multiple(self,
+                               tokens: List[str],
+                               pad_to_size: Optional[int] = None,
+                               padding_element: int = 0) -> List[int]:
+        if pad_to_size is not None:
+            tokens = tokens[:pad_to_size]
+
+        ids = [self.add_or_get_id(t) for t in tokens]
+
+        if pad_to_size is not None and len(ids) != pad_to_size:
+            ids += [padding_element] * (pad_to_size - len(ids))
+
+        return ids
+
+    def get_id_or_unk_multiple(self,
+                               tokens: List[str],
+                               pad_to_size: Optional[int] = None,
+                               padding_element: int = 0) -> List[int]:
+        if pad_to_size is not None:
+            tokens = tokens[:pad_to_size]
+
+        ids = [self.get_id_or_unk(t) for t in tokens]
+
+        if pad_to_size is not None and len(ids) != pad_to_size:
+            ids += [padding_element] * (pad_to_size - len(ids))
+
+        return ids
+
+    def get_name_for_id(self, token_id: int) -> str:
+        return self.id_to_token[token_id]
+
+    def __len__(self) -> int:
+        return len(self.token_to_id)
+
+    def __str__(self):
+        return str(self.token_to_id)
+
+    def get_all_names(self) -> FrozenSet[str]:
+        return frozenset(self.token_to_id.keys())
+
+    def translate_tokenized_array_to_list_words(self, token: np.ndarray) -> List[str]:
+        """Helper function to translate numpy array tokens back to words"""
+        return [self.get_name_for_id(n) for n in token[np.nonzero(token != self.get_id_or_unk(self.get_pad()))]]
+
+    @staticmethod
+    def get_unk() -> str:
+        return '%UNK%'
+
+    @staticmethod
+    def get_pad() -> str:
+        return '%PAD%'
